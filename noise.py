584faf1f39afeb1dfd44927f421e577efd25ce44
jmcrego <jmcrego@gmail.com>
2023-03-30 21:44:02 +0000

    light

--- noise.py
+++ noise.py
@@ -11,8 +11,8 @@ from noiser.Noiser import Noiser
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--inflect', type=str, default=None, help="inflection replacements (None)")
-    parser.add_argument('--hphone', type=str, default=None, help="homophone replacements (None)")
+    parser.add_argument('-inflect', type=str, default=None, help="inflection replacements (None)")
+    parser.add_argument('-hphone', type=str, default=None, help="homophone replacements (None)")
     parser.add_argument('-max_occ', type=int, default=100, help='max occurrences for the pair original/noised word (100)')
     parser.add_argument('-max_ratio', type=float, default=0.5, help='max ratio noises per sentence (0.5)')
     parser.add_argument('-max_total', type=int, default=5, help='max total noises per sentence (5)')
